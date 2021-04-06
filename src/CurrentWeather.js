e323d8727903939ee54eba8e98c75e9c9fed3050
stephepush <7speter@gmail.com>
2021-04-06 12:03:55 -0400

    Establishes initial intended functionality between getLocation and useEffect hook

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -4,6 +4,9 @@ export default function CurrentWeather() {
     const [lat, setLat] = useState(null)
     const [lng, setLng] = useState(null)
     const [status, setStatus] = useState(null)
+    const [data, setData] = useState(null)
+    const MARSUPIAL = '26db1c8d52944d17bac192316210404&'
+    let callLink = `https://api.weatherapi.com/v1/current.json?key=${MARSUPIAL}q=${lat},${lng}`
 
     const getLocation = () => {
         if (!navigator.geolocation) {
@@ -19,18 +22,26 @@ export default function CurrentWeather() {
             });
         }
     }
+    
+    
 
     useEffect(() => {
         getLocation()
+        
+        return fetch(callLink)
+            .then(res => res.json())
+            .then(response => setData(response))
+            .then(console.log(data))
     }, [])
 
     return (
         <div >
             
-            <h1>Current Weather In LOCATION</h1>
+            <h1>Current Weather In {data.location.name}</h1>
             {status}
             {lat && <p>Latitude: {lat}</p>}
             {lng && <p>Longitude: {lng}</p>}
+            {data.current.temp_f}
         </div>
     )
 }
