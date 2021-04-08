1911080316435fb2ecc795953545f17b9c058096
stephepush <7speter@gmail.com>
2021-04-07 22:41:28 -0400

    Splits useEffect hook into 2, gets things working

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -6,7 +6,7 @@ export default function CurrentWeather() {
     const [status, setStatus] = useState(null)
     const [data, setData] = useState(null)
     const MARSUPIAL = '26db1c8d52944d17bac192316210404&'
-    let callLink = `https://api.weatherapi.com/v1/current.json?key=${MARSUPIAL}q=${lat},${lng}`
+    //let callLink = `https://api.weatherapi.com/v1/current.json?key=${MARSUPIAL}q=${lat},${lng}`
 
     const getLocation = () => {
         if (!navigator.geolocation) {
@@ -33,13 +33,17 @@ export default function CurrentWeather() {
     useEffect(() => {
         getLocation()
         
-        return fetch(callLink)
-                    .then(res => res.json())
-                    .then(response => setData(response))
-                    .then(console.log(data))
-        // fetchCall()
-        
-    }, [callLink])
+    }, [])
+
+    useEffect(() => {
+        let callLink = `https://api.weatherapi.com/v1/current.json?key=${MARSUPIAL}q=${lat},${lng}` 
+        if (lat && lng) {
+            return fetch(callLink)
+        .then((res) => res.json())
+        .then((response) => setData(response));
+      // fetchCall()
+        }
+    }, [lat, lng]);
 
     return (
         <div >
@@ -48,7 +52,7 @@ export default function CurrentWeather() {
             {status}
             {lat && <p>Latitude: {lat}</p>}
             {lng && <p>Longitude: {lng}</p>}
-            {/* {data.current.temp_f} */}
+            {data.current.temp_f}
         </div>
     )
 }
